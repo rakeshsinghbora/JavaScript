f0434c9aa0adea64c52f598e3af500e889782a13
Rakesh Singh Bora <80658380+rakeshsinghbora@users.noreply.github.com>
2025-04-03 20:37:45 +0530

    Implemented tabbed in our app

--- DOMManipulation/script.js
+++ DOMManipulation/script.js
@@ -8,11 +8,21 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const btnScroll = document.querySelector('.btn--scroll-to');
+const section1 = document.getElementById('section--1');
+
+const navLink = document.querySelectorAll('.nav__link');
+const navLinks = document.querySelector('.nav__links');
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-
 };
 
 const closeModal = function () {
@@ -20,8 +30,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn=> btn.addEventListener('click', openModal));
-
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -32,25 +41,52 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-
-
-//Smooth Scrolling 
-
-const btnScroll=document.querySelector('.btn--scroll-to');
-const section1=document.getElementById('section--1');
-
-btnScroll.addEventListener('click',(e)=>{
-
+//Smooth Scrolling
+btnScroll.addEventListener('click', e => {
   //Modern way compatible with new Browser
   // section1.scrollIntoView({behavior:'smooth'});
 
-
   //Old School
-  const section1Coordinates=section1.getBoundingClientRect();
+  const section1Coordinates = section1.getBoundingClientRect();
 
   window.scrollTo({
-    left:section1Coordinates.left +window.pageXOffset,
-    top:section1Coordinates.top +window.pageYOffset,
-    behavior:'smooth'
+    left: section1Coordinates.left + window.pageXOffset,
+    top: section1Coordinates.top + window.pageYOffset,
+    behavior: 'smooth',
   });
+});
+
+//Nav links
+
+navLinks.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// navLink.forEach(function (nav){
+//   nav.addEventListener('click',function (e){
+//     e.preventDefault();
+
+//     const id=this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({behavior:'smooth'});
+//   })
+// })
+
+
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if(!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
